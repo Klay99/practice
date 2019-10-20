049ffcdd4291a3d096f075c49b108fed8bd6ea38
Koty <Koty1209@qq.com>
2019-10-20 09:34:19 +0800

    the repeating number in an array

--- src/nowcoder/_3/Solution.java
+++ src/nowcoder/_3/Solution.java
@@ -10,6 +10,28 @@ package nowcoder._3;
  **/
 public class Solution {
 
+    public boolean DuplicationArray(int[] arr, int res){
 
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+
+        return false;
+    }
+
+    public void print(int[] arr){
+        System.out.print("[");
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i]+",");
+        }
+        System.out.println(arr[arr.length-1]+"]");
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] a = {2,1,3,0,2,5,3};
+        s.DuplicationArray(a,2);
+        s.print(a);
+    }
 
 }
