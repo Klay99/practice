7c8031aed3b925d997e613088e8b6a012afdbbb2
Koty <Koty1209@qq.com>
2019-10-20 09:32:53 +0800

    the repeating number in an array

--- src/nowcoder/_9/Solution.java
+++ src/nowcoder/_9/Solution.java
@@ -1,10 +1,29 @@
 package nowcoder._9;
 
+import java.util.Stack;
+
 /**
  * @program: practice
- * @description: 从头到尾打印链表
+ * @description: 用两个栈实现队列
  * @author: Koty
  * @create: 2019-10-16 16:09
  **/
 public class Solution {
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
 }
