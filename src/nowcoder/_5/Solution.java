2609bec87c25107b0beb51962e9f65a288237310
Koty <Koty1209@qq.com>
2019-10-20 09:39:17 +0800

    replace blank

--- src/nowcoder/_5/Solution.java
+++ src/nowcoder/_5/Solution.java
@@ -8,17 +8,5 @@ package nowcoder._5;
  **/
 public class Solution {
 
-    public String replaceSpace(StringBuffer str) {
-        String res = "";
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ' ') {
-                res += "%20";
-            } else {
-                res += str.charAt(i);
-            }
-        }
-
-        return res;
-    }
 
 }
