c73dbf2c9847e367d58e7b040a48ae7484cf6378
Koty <Koty1209@qq.com>
2019-10-25 19:58:01 +0800

    merge two sorted linked lists

--- src/nowcoder/ListNode.java
+++ src/nowcoder/ListNode.java
@@ -7,11 +7,27 @@ package nowcoder;
  * @create: 2019-09-25 14:58
  **/
 public class ListNode {
-    int val;
-    ListNode next;
+    private int val;
+    private ListNode next;
 
     ListNode(int val) {
         this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
     }
 
     @Override
--- src/nowcoder/_25/Solution.java
+++ src/nowcoder/_25/Solution.java
@@ -2,9 +2,59 @@ package nowcoder._25;
 
 /**
  * @program: practice
- * @description: 从头到尾打印链表
+ * @description: 合并两个排序的链表
+ *               输入两个单调递增的链表，输出两个链表合成后的链表，
+ *               当然我们需要合成后的链表满足单调不减规则。
  * @author: Koty
  * @create: 2019-10-16 16:09
  **/
 public class Solution {
+
+    class ListNode {
+        int val;
+        ListNode next = null;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode Merge(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        } else if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+        ListNode curr1 = list1;
+        ListNode curr2 = list2;
+        ListNode res, currRes;
+        if (curr1.val < curr2.val) {
+            res = curr1;
+            curr1 = curr1.next;
+        } else {
+            res = curr2;
+            curr2 = curr2.next;
+        }
+        currRes = res;
+        while (curr1 != null && curr2 != null) {
+            if (curr1.val < curr2.val) {
+                currRes.next = curr1;
+                curr1 = curr1.next;
+            } else {
+                currRes.next = curr2;
+                curr2 = curr2.next;
+            }
+            currRes = currRes.next;
+        }
+        if (curr1 != null) {
+            currRes.next = curr1;
+        }
+        if (curr2 != null) {
+            currRes.next = curr2;
+        }
+
+        return res;
+    }
+
 }
