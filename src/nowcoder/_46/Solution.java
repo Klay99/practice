469a90b0c4b8d59f8c85ea9712c16762f6e0e72a
Koty <Koty1209@qq.com>
2019-12-01 14:38:29 +0800

    translate Numbers into strings

--- src/nowcoder/_46/Solution.java
+++ src/nowcoder/_46/Solution.java
@@ -2,9 +2,18 @@ package nowcoder._46;
 
 /**
  * @program: practice
- * @description: 从头到尾打印链表
+ * @description: 把数字翻译成字符串
+ *               给定一个数字，我们按照如下规则把它翻译为字符串：0翻译成“a”，1翻译成“b”，…，25翻译成“z”。
+ *               一个数字可能有多个翻译。例如，12258有5种不同的翻译，
+ *               分别是“bccfi”、“bwfi”、“bczi”、“mcfi”、“mzi”。
+ *               请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
  * @author: Koty
  * @create: 2019-10-16 16:09
  **/
 public class Solution {
+
+    public int getTranslationCount(int number) {
+
+    }
+
 }
